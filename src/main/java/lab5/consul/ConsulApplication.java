e1fb2678eebecc2f155fa3a717afc2b06b5b8e02
yaskalnazar <yaskalnazar@gmail.com>
2020-06-21 15:07:13 +0300

    Init commit

---
+++ src/main/java/lab5/consul/ConsulApplication.java
@@ -0,0 +1,13 @@
+package lab5.consul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulApplication.class, args);
+	}
+
+}
